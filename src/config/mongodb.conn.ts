f2675def15c5da6d2bc2f16e626ae6913131b056
Abdul Wahab <abwahab958@gmail.com>
2021-09-16 00:54:21 -0700

    Done

--- src/config/mongodb.conn.ts
+++ src/config/mongodb.conn.ts
@@ -3,10 +3,10 @@ export class DbMongo {
   constructor() {
   }
   connect(h: string, dbName: string, u?: string, pass?: string, p?: number) {
-    let connectionuri = `mongodb+srv://wahab:Ahhad123@cluster0.gnqnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-    // if (u != undefined && pass != undefined) {
-    //   connectionuri = `mongodb+srv://${u}:${pass}@${h}/${dbName}`;
-    // }
+    let connectionuri = `mongodb://${h}:${p}/${dbName}`;
+    if (u != undefined && pass != undefined) {
+      connectionuri = `mongodb+srv://${u}:${pass}@${h}/${dbName}`;
+    }
     connect(connectionuri, (err: any) => {
       if (err) {
         console.log(err);
--- src/utills/constant.ts
+++ src/utills/constant.ts
@@ -3,4 +3,4 @@ export const dbHost: string = 'cluster0.gnqnx.mongodb.net'
 export const dbUser: string = 'wahab'
 export const dbPassword: string = 'Ahhad123'
 
-// mongodb+srv://wahab:Ahhad123@cluster0.gnqnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// mongodb+srv://wahab:Ahhad123@cluster0.gnqnx.mongodb.net/myFirstDatabase?retryWri
